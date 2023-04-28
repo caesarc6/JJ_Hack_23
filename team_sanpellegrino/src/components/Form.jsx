d10187251fb2269ed30513676cd22c7b4d255672
ItsRamee <ramee.yudeh@gmail.com>
2023-04-28 17:06:51 -0400

    fixed

--- team_sanpellegrino/src/components/Form.jsx
+++ team_sanpellegrino/src/components/Form.jsx
@@ -67,15 +67,12 @@ function Form() {
             <td>{item.pm10}</td>
             <td>{item.o3}</td>
             <td>
-              {item.aqi >= 0 && item.aqi <= 50 ? (
-                "Good"
-              ) &&  : item.aqi >= 51 && item.aqi <= 100 ? (
-                "Moderate"
-              ) : item.aqi >= 101 && item.aqi <= 150 ? (
-                "Bad"
-              ) : (
-                ""
-              )}
+              {
+                item.aqi >= 0 && item.aqi <= 50 ? ("Good"): 
+                item.aqi >= 51 && item.aqi <= 100 ? ("Moderate"): 
+                item.aqi >= 101 && item.aqi <= 150 ? ("Bad"): 
+                ("")
+              }
             </td>
           </tr>
         ))}
