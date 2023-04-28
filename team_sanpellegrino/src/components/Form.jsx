e153147df33bf8ced07818d0edb0fad5426939f1
caesar <caesar.cuzco@jjay.cuny.edu>
2023-04-28 13:23:38 -0400

    nav bar center

--- team_sanpellegrino/src/components/Form.jsx
+++ team_sanpellegrino/src/components/Form.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
-    const handleSubmit = (e) => {
-        console.log(e)
-    }
+  const handleSubmit = (e) => {
+    console.log(e);
+  };
 
-    return(
-        <div>
-            <form id='form_location' onSubmit={handleSubmit}>
-                <input
-                    name='location_input'
-                    type='text'
-                    id='location_input'
-                    placeholder='Enter a location'
-                    className='location_input_bar'
-                />
-                <button type='submit'
-                    id='submit-btn'
-                    form='form_location'
-                    >
-                    Search
-                </button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form id="form_location" onSubmit={handleSubmit}>
+        <input
+          name="location_input"
+          type="text"
+          id="location_input"
+          placeholder="Enter a location"
+          className="location_input_bar"
+        />
+        <button type="submit" id="submit-btn" form="form_location">
+          Get Weather
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
