387affac1db9e681889ecb2c034553dc61a9eb7e
ItsRamee <ramee.yudeh@gmail.com>
2023-04-28 13:56:28 -0400

    he

--- team_sanpellegrino/src/components/Form.jsx
+++ team_sanpellegrino/src/components/Form.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Form() {
   const handleSubmit = (e) => {
+    
+    function WeatherDisplay({ city }) {
+    const [weatherData, setWeatherData] = useState(null);
+    const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [city]);
+
+  if (error) {
+    return <div>Failed to load weather data: {error.message}</div>;
+  }
+
+  if (!weatherData) {
+    return <div>Loading weather data...</div>;
+  }
+
+  const weatherString = JSON.stringify(weatherData);
+  return <div id="Display_Weather">{weatherString}</div>;
+}
+
+
     console.log(e);
   };
 
