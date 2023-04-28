9d2ca8195c8dd5e8548aac7e1b4923a8f6e37431
caesar <caesar.cuzco@jjay.cuny.edu>
2023-04-28 14:21:40 -0400

    url change

--- team_sanpellegrino/src/components/WeatherDisplay.jsx
+++ team_sanpellegrino/src/components/WeatherDisplay.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 function WeatherDisplay({ city }) {
-    const [weatherData, setWeatherData] = useState(null)
-    const [error, setError] = useState(null)
-    
-    useEffect(() => {
-      const fetchWeatherData = async () => {
-        try {
-          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`;
-          const response = await fetch(url);
-          const data = await response.json();
-          setWeatherData(data);
-        } catch (error) {
-          setError(error);
-        }
-      };
-  
-      fetchWeatherData();
-    }, [city]);
-  
-    if (error) {
-      console.log("Failed to load weather data")
-    }
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
 
-    if (!weatherData) {
-      console.log("Loading weather data...")
-    }
-  
-    const weatherString = JSON.stringify(weatherData);
-    return <div id="Display_Weather">{weatherString}</div>;
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const url = `https://api.weatherbit.io/v2.0/forecast/airquality?&postal_code=${city}&key=3140a681d3b34703bdbe0d30c8ddab50`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [city]);
+
+  if (error) {
+    console.log("Failed to load weather data");
   }
 
-export default WeatherDisplay
+  if (!weatherData) {
+    console.log("Loading weather data...");
+  }
+
+  const weatherString = JSON.stringify(weatherData);
+  return <div id="Display_Weather">{weatherString}</div>;
+}
+
+export default WeatherDisplay;
