2554cd797c34a48689f0d9daebe074603ea2363d
Britney <Wu.Britney@gmail.com>
2023-04-28 16:12:24 -0400

    created red bg result and styling

---
+++ team_sanpellegrino/src/components/Negative.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Negative () {
+    return(
+        <div class='negative-bg'>
+            <h1>Bad Air Quality</h1>
+        </div>
+    )
+}
+
+export default Negative
