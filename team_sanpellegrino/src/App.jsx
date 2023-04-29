b141ffd4822bf0985245020ffc2229a5aaa280e6
Britney <Wu.Britney@gmail.com>
2023-04-29 10:47:24 -0400

    Created Intro text

--- team_sanpellegrino/src/App.jsx
+++ team_sanpellegrino/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import DrawerAppBar from "./components/DrawerAppBar";
+import Intro from './components/Intro'
 import Form from "./components/Form";
 
 function App() {
   return (
     <>
       <DrawerAppBar />
+      <Intro />
       <Form />
     </>
   );
---
+++ team_sanpellegrino/src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Intro() {
+    return(
+        <div>
+            <h1>Social change for clean air</h1>
+            <h2>Find out how the air in your city impacts you</h2>
+        </div>
+    )
+}
+
+export default Intro
