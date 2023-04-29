d1496f96f0c34393232c9d5c939ac719268e1eb5
Britney <Wu.Britney@gmail.com>
2023-04-29 11:58:31 -0400

    created learnmore text & bg

--- team_sanpellegrino/src/components/LearnMore.jsx
+++ team_sanpellegrino/src/components/LearnMore.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 function LearnMore() {
     return(
-        <div>
-            <h1>LearnMore page</h1>
+        <div class="learn-more-page-container">
+            <h1>Learn More: The Health and Climate Impacts</h1>
+            <h3>Today, we start a change. The air we breathe impacts our health and environment.</h3>
+            <div>
+                <div>
+                    <h2>Health Impact</h2>
+                    <p>Air pollution is hard to escape, regardless of how rich an area you live in. It is all around us. Tiny and invisible pollutants in the air can sneak past our body’s defenses, penetrating deep into our respiratory and circulatory system. This results in damage to our lungs, hearts and brains.</p>
+                    <p>According to the World Health Organization: "As the world gets hotter and more crowded, our engines continue to pump out dirty emissions, and half the world has no access to clean fuels or technologies (e.g. stoves, lamps), the very air we breathe is growing dangerously polluted: nine out of ten people now breathe polluted air, which kills 7 million people every year."</p>
+                </div>
+                <div>
+                    <h2>Climate Impact</h2>
+                    <p>Air pollution is also connected to climate change. Burning of fossil fuels release C02 into the air. This results in:</p>
+                        <li>Rising temperatures</li>
+                        <li>Snow and ice melt</li>
+                        <li>Crop damage</li>
+                </div>
+            </div>
         </div>
     )
 }
