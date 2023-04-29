2b96abe18af361d40f99a752c0cb63a97fb34694
Britney <Wu.Britney@gmail.com>
2023-04-29 12:28:50 -0400

    resources content

--- team_sanpellegrino/src/components/Resources.jsx
+++ team_sanpellegrino/src/components/Resources.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Resources() {
     return(
-        <h1>Resources page</h1>
+        <div>
+            <h1>Resources</h1>
+            <ul>
+                <li>
+                    <a href="https://www.epa.gov/outdoor-air-quality-data/air-quality-statistics-report" target="_blank" rel="noopener noreferrer">Look at the U.S.A. Environmental Protection Agency Statistical reports</a>
+                </li>
+                <li>
+                    <a href="https://www.who.int/news-room/spotlight/how-air-pollution-is-destroying-our-health" target="_blank" rel="noopener noreferrer">World Health Organization - How Air Pollution is Destroying Our Health</a>
+                </li>
+            </ul>
+
+        </div>
+        
     )
 }
 
