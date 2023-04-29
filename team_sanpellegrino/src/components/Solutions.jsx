c5088b1ff6cfba4cee1bdbf728b2fb4de4437c75
ItsRamee <ramee.yudeh@gmail.com>
2023-04-29 13:48:01 -0400

    rysolution

--- team_sanpellegrino/src/components/Solutions.jsx
+++ team_sanpellegrino/src/components/Solutions.jsx
@@ -2,24 +2,16 @@ import React from 'react'
 
 function Solutions() {
     return(
-        <div>
-            <h1>Solutions</h1>
-            <h2 style={{ fontFamily: 'Arial' }}>Some solutions you can take!</h2>
-            <div style={{ textAlign: 'center' }}>
-            <p contentEditable style={{ fontFamily: 'Arial', margin: '40px auto' }}>
-                Take a bike ride! Or save money on that Uber and just take public transportation! Or restrict
-                on anything that has to do with transportation of goods. Transportation is the largest
-                source of greenhouse gasses, so maybe order less Amazon prime boxes and go to store!
-            </p>
-            <p contentEditable style={{ fontFamily: 'Arial', margin: '40px auto' }}>
-                RECYCLE ALWAYS! Do you have endless amounts of empty water bottles in your room? Recycle those bottles 
+        <div className='data-box2' style={{ textAlign: 'center' }}>
+            <h1>Solutions you can take!</h1>
+            <p contentEditable style={{ fontFamily: 'Arial', margin: '100px auto' }}>
+                <h3>Take a bike ride! Or save money on that Uber and just take public transportation! Or restrict
+                on anything that has to do with transportation of goods.Transportation is the largest
+                source of greenhouse gasses, so maybe order less Amazon prime boxes and go to store!</h3>
+                <h3>RECYCLE ALWAYS! Do you have endless amounts of empty water bottles in your room? Recycle those bottles 
                 into something useful! Maybe make a flower pot, or use it store excessive kitchen necessities,
-                don't throw out anything you can use again.
+                don't throw out anything you can use again.</h3>
             </p>
-            <p contentEditable style={{ fontFamily: 'Arial', margin: '40px auto' }}>
-                Editable paragraph 3
-            </p>
-            </div>
         </div>
     )
 }
