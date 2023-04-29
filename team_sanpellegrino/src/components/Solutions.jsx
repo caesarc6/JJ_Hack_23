d60220635cd6b68e7d036bc68852e7f852ede807
ItsRamee <ramee.yudeh@gmail.com>
2023-04-29 12:48:38 -0400

    ramee s3

--- team_sanpellegrino/src/components/Solutions.jsx
+++ team_sanpellegrino/src/components/Solutions.jsx
@@ -3,18 +3,22 @@ import React from 'react'
 function Solutions() {
     return(
         <div>
-            <h1>Solution page</h1>
+            <h1>Solutions</h1>
             <h2 style={{ fontFamily: 'Arial' }}>Some solutions you can take!</h2>
             <div style={{ textAlign: 'center' }}>
-            <p contentEditable style={{ fontFamily: 'Arial', margin: '30px auto' }}>
+            <p contentEditable style={{ fontFamily: 'Arial', margin: '40px auto' }}>
                 Take a bike ride! Or save money on that Uber and just take public transportation! Or restrict
                 on anything that has to do with transportation of goods. Transportation is the largest
                 source of greenhouse gasses, so maybe order less Amazon prime boxes and go to store!
             </p>
-            <p contentEditable style={{ fontFamily: 'Arial', margin: '20px auto' }}>
-                
+            <p contentEditable style={{ fontFamily: 'Arial', margin: '40px auto' }}>
+                RECYCLE ALWAYS! Do you have endless amounts of empty water bottles in your room? Recycle those bottles 
+                into something useful! Maybe make a flower pot, or use it store excessive kitchen necessities,
+                don't throw out anything you can use again.
             </p>
-            <p contentEditable style={{ fontFamily: 'Arial', margin: '20px auto' }}>Editable paragraph 3</p>
+            <p contentEditable style={{ fontFamily: 'Arial', margin: '40px auto' }}>
+                Editable paragraph 3
+            </p>
             </div>
         </div>
     )
