f72d1a69c891d77fa0c01f418042b0c6a462116f
Britney Wu <Wu.Britney@gmail.com>
2024-03-28 13:57:28 -0400

    Edit Intro text

--- team_sanpellegrino/src/components/Intro.jsx
+++ team_sanpellegrino/src/components/Intro.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Intro() {
     return(
         <div>
-            <h1>Social change for clean air</h1>
-            <h2>Find out how the air in your city impacts you</h2>
+            <h1>Find out how the air in your city impacts you!</h1>
         </div>
     )
 }
